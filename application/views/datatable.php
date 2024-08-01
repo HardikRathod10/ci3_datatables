909b519b44c1e1f0c79c67e1393641bc783761dc
hardik rathod <hardikvrathod1112@gmail.com>
2024-08-01 18:06:13 +0530

    remote merged.

--- application/views/datatable.php
+++ application/views/datatable.php
@@ -1,21 +1,43 @@
 <?php $this->load->view("header"); ?>
 <div class="container my-4 p-4">
     <table class="table" id="example">
-        <select class="form-select w-25 float-end mb-3" name="gender" id="gender">
-            <option value="">Select Gender</option>
-            <option value="M">Male</option>
-            <option value="F">Female</option>
-        </select>
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">First Name</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">Gender</th>
-                <th scope="col">Hide Data</th>
-            </tr>
-        </thead>
-        <!-- <tbody>
+        <div class="d-flex align-items-center justify-content-between mb-2">
+            <select class="form-select form-select-sm w-25" name="gender" id="gender">
+                <option value="">Select Gender</option>
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+            </select>
+            <div class="d-flex">
+                <div class="d-flex align-items-center me-4">
+                    <label for="sdate">Start:</label>
+                    <input type="date" class="form-control form-control-sm" name="sdate" id="sdate"
+                        placeholder="Start date">
+                </div>
+                <div class="d-flex align-items-center me-4">
+                    <label for="sdate">End:</label>
+                    <input type="date" class="form-control form-control-sm" name="edate" id="edate"
+                        placeholder="End date">
+                </div>
+                <div class="btn-group">
+                    <button class="btn btn-secondary btn-sm" id="apply-btn">Apply</button>
+                    <button class="btn btn-secondary btn-sm" id="reset-btn">Reset</button>
+                </div>
+            </div>
+        </div>
+</div>
+<thead>
+    <tr>
+        <th scope="col">#</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Birth Date</th>
+        <th scope="col">Department</th>
+        <th scope="col">Gender</th>
+        <th scope="col">Hide Date</th>
+        <th scope="col">Action</th>
+    </tr>
+</thead>
+<!-- <tbody>
             <tr>
                 <th scope="row"></th>
                 <td></td>
@@ -24,6 +46,6 @@
                 <td></td>
             </tr>
         </tbody> -->
-    </table>
+</table>
 </div>
 <?php $this->load->view("footer"); ?>
