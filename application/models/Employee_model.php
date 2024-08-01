840f0819d81d109f2518d5ad075c6d2b138a5202
hardik rathod <hardikvrathod1112@gmail.com>
2024-08-01 18:27:26 +0530

    Reset filter option added.

--- application/models/Employee_model.php
+++ application/models/Employee_model.php
@@ -7,11 +7,16 @@ class Employee_model extends CI_Model
     {
         parent::__construct();
     }
-
+    // Method to get all employees
     public function get_employees()
     {
         $this->db->limit(30);
         return $this->db->get('employee')->result();
+    }
+    // Method to get all departments
+    public function get_departments()
+    {
+        return $this->db->get('department')->result();
     }
     // Counting all records
     public function count_all()
@@ -19,42 +24,48 @@ class Employee_model extends CI_Model
         return $this->db->count_all('employee');
     }
     // Method to count filtered records which will be passed at recordsFiltered option 
-    public function count_filtered()
+    public function count_filtered($post_data)
     {
-        $this->make_query();
+        $this->make_query($post_data);
         return $this->db->get()->num_rows();
     }
 
     // Method to generate query
-    public function make_query()
+    public function make_query($post_data)
     {
-        $this->db->select();
+        $this->db->select('employee.*, department.dept_name as dept_name');
         $this->db->from('employee');
+        $this->db->join('dept_emp', 'employee.emp_no = dept_emp.emp_no');
+        $this->db->join('department', 'dept_emp.dept_no=department.dept_no');
         // Adding like query
-        if ($this->input->post()['search']['value'] != "" || $this->input->post()['gender'] != '') {
-            // $like_arr = [
-            //     'first_name' => $this->input->post()['search']['value'],
-            //     'last_name' => $this->input->post()['search']['value'],
-            //     'gender' => $this->input->post()['gender'],
-            // ];
-            $this->db->like('first_name',$this->input->post()['search']['value']);
-            $this->db->or_like('last_name',$this->input->post()['search']['value']);
-            $this->db->like('gender',$this->input->post()['gender']);
+        if ($post_data['search']['value'] != "") {
+            $this->db->or_like('first_name', $post_data['search']['value']);
+            $this->db->or_like('last_name', $post_data['search']['value']);
+        }
+        // Filtering employees based on gender
+        if (!empty($post_data['gender'])) {
+            $this->db->where('gender', $post_data['gender']);
+        }
+        // Filtering employees between specified birth dates
+        if (!empty($post_data['sdate']) && !empty($post_data['edate'])) {
+            $this->db->where("birth_date BETWEEN '{$post_data['sdate']}' AND '{$post_data['edate']}'");
+        }
+        if (!empty($post_data['order'][0]['name']) && !empty($post_data['order'][0]['dir'])) {
+            $this->db->order_by($post_data['order'][0]['name'], $post_data['order'][0]['dir']);
+        }
+        else{
+            $this->db->order_by('emp_no', 'asc');
         }
     }
 
     // Method to built datatable
-    public function get_datatable()
+    public function get_datatable($post_data)
     {
-        $this->make_query();
-        $order_column = isset($this->input->post()['order'][0]['name']) ? $this->input->post()['order'][0]['name'] : "emp_no";
-        $order_direction = isset($this->input->post()['order'][0]['dir']) ? $this->input->post()['order'][0]['dir'] : "ASC";
-        $this->db->order_by($order_column, $order_direction);
-        if ($this->input->post()['length'] != -1) {
-            $this->db->limit($this->input->post()['length'], $this->input->post()['start']);
+        $this->make_query($post_data);
+        if ($post_data['length'] != -1) {
+            $this->db->limit($post_data['length'], $post_data['start']);
         }
         return $this->db->get()->result();
     }
-
 }
 ?>
